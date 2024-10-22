8aa0436aa9280e8cb36cd62201160b5d414c3d69
SMohanapriya2174 <www.mohanapriya004@gmail.com>
2024-10-22 09:46:40 +0530

    firstcommit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1>MOHANAPRIYA S</h1>
+
+        <h2>Contact information</h2>
+
+        <p>Address:Rasipuram,Namakkal-636113
+          <br />
+          Email id:mohanapriyamp2174@gmail.com
+          <br />
+          Mobile:6380160200
+        </p>
+        <h2>Summary</h2>
+        <p>Highly motivated and detailed oriented computer science engineer with a strong academic
+          record.Skilled in web development and operating system.Proeficient in programming languages
+          such as HTML/CSS,C programming.Looking to join a dynamic team and grow as a software
+          professional.
+        </p>
+
+        <h2>Educaation</h2>
+        <p>Bachelor of Enginnering in Computer Science and Engineering
+          <br />
+          Vetri Vinayaha College of Engineering and Technology
+
+        </p>
+        <h2>Skills</h2>
+        <h3>Technical skills </h3>
+        .Web development
+        <br />
+        .Data structures and Algorithms
+        <br />
+        .Git
+
+        <h3>Soft skills</h3>
+        .Communication
+        <br />
+        .Team work
+
+        <h2>Certification</h2>
+        <p>.Full stack development <br />
+          .C programming
+        </p>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
